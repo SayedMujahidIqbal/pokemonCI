68d49d9e39e483f37ab50e0d544fa6ed08a29a2e
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2025-02-18 12:45:33 +0300

    cyrpess integration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      config.typescript = false
+      return config
+    },
     baseUrl: 'http://localhost:8080',
     env: {
       BACKEND: 'http://localhost:5000',
