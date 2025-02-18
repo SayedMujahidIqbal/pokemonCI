cda4a201bb0957f6d1bf389eae2e7be5f26c00b5
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2025-02-18 15:13:45 +0300

    cypress integration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      config.chromeWebSecurity = false
+      return config
+    },
     baseUrl: 'http://localhost:8080',
     env: {
       BACKEND: 'http://localhost:5000',
